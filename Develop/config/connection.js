accb186d08112e80c869e119a0cc719d87de1e96
Trevor Hilimire <trevorhilimire@gmail.com>
2022-06-18 15:13:46 -0400

    adds production config for heroku

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize =
+// const sequelize =
 
-new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+// new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
 
-module.exports = sequelize;
+// module.exports = sequelize;
