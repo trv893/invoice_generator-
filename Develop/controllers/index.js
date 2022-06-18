c8d561ff0030819df33dd08ee89779ba684b2e83
Trevor Hilimire <trevorhilimire@gmail.com>
2022-06-18 14:48:44 -0400

    removes config.json from gitignore and revers paths

--- Develop/controllers/index.js
+++ Develop/controllers/index.js
@@ -1,8 +1,8 @@
 const path = require('path');
 const router = require('express').Router();
-const db_customer_routes = require(path.join(__dirname,'./db_customer_routes'));
-const db_invoice_routes = require(path.join(__dirname,'./db_invoice_routes'));
-const db_proposal_routes = require(path.join(__dirname,'./db_proposal_routes'));
+const db_customer_routes = require(path.join(__dirname,'/db_customer_routes'));
+const db_invoice_routes = require(path.join(__dirname,'/db_invoice_routes'));
+const db_proposal_routes = require(path.join(__dirname,'/db_proposal_routes'));
 
 // expose api routes to the client
 router.use('/api', db_customer_routes);
