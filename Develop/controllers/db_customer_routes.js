b24c3cb2000f4c6341ca3769336717fef3390a18
Trevor Hilimire <trevorhilimire@gmail.com>
2022-06-18 13:32:44 -0400

    get customer api

--- Develop/controllers/db_customer_routes.js
+++ Develop/controllers/db_customer_routes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
-const { Cusomters } = require('../models');
-
+var db = require('../models');
+var initModels = require("../models/init-models");
+var models = initModels(db.sequelize);
 
 router.get('/customer', async (req, res) => {
     // Send the rendered Handlebars.js template back as the response
-    res.status(200).json("success")
+    var c = await models.dbo_customers.findAll();
+
+    res.status(200).json(c)
   });
   
 module.exports = router;
--- Develop/db_service/index.js
+++
@@ -1,5 +0,0 @@
-const db_customer_table = require('../models/dbo_customers')
-
-
-
-module.exports = {db_customer_table}
