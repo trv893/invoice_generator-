50c98f25cd4dc3288e1b09ac0b1f78f3b56ffae2
Trevor Hilimire <trevorhilimire@gmail.com>
2022-06-27 11:41:19 -0400

    Lode env vars from process instead of file

--- app_config.js
+++ app_config.js
@@ -1,14 +1,14 @@
 // npm module that loads enviroment variables from a .env file
-const evnvar =require('dotenv').config()
+require('dotenv').config()
 
 const myconfig ={
-      "host" : evnvar.parsed.DB_HOST,
-      "database": evnvar.parsed.DB_NAME,
-      "username": evnvar.parsed.DB_USER,
-      "password": evnvar.parsed.DB_PW,
+      "host" : process.env.DB_HOST,
+      "database": process.env.DB_NAME,
+      "username": process.env.DB_USER,
+      "password": process.env.DB_PW,
       "use_env_variable": false,
       "dialect": "mysql",
-      "authRedirectUri": evnvar.parsed.AUTH_0_REDIRECT
+      "authRedirectUri":process.env.AUTH_0_REDIRECT
   };
 
 
